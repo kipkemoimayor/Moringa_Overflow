483c540cd89b7d97a35e44ebb890636fd00a1f32
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-29 21:44:05 +0300

    Added test case for users class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 '''
 table for users
@@ -11,6 +11,17 @@ class Users(db.Model):
     email=db.Column(db.String(255))
     pass_secure=db.Column(db.String(255))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError("You cannot red the password attribute")
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure =generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 
     def __repr__(self):
--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import Users,Role
+from  flask_migrate import Migrate, MigrateCommand
 
 app=create_app("development")
 manager=Manager(app)
 manager.add_command("server",Server)
-
+migrate=Migrate(app,db)
+Manager.add_command('db',MigrateCommand)
 
 
 
