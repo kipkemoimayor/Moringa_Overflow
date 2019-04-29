695d86d189e231267345263bde16323dbd836e7a
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-29 22:45:23 +0300

    Added a logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,44 @@
-from flask import render_template
 from .import auth
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_required,login_user,login_user
+from .. import db
+from ..models import Users
 
+
+'''
+login users
+'''
 @auth.route("/login")
 def login():
+    loginForm='form'
+    if loginForm.validate_on_submit():
+        user = Users.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-    
-    return render_template("auth/login.html")
+        flash('Invalid username or Password')
+
+    title = "Moringa flow login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+#registration
+@auth.route("/register")
+def register():
+    form="form name"
+    if form.validate_on_submit():
+        roles="Users";
+        user=Users(email=form.email.data,username=form.username.data,password=form.password.data,role_id=roles)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("auth.login"))
+
+    return render_template("auth/register",form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/main/views.py
+++ app/main/views.py
@@ -10,24 +10,3 @@ def index():
     title="Welcome to Moringa flow"
 
     return render_template("index.html",title=title)
-
-
-#registration
-@main.route("/register")
-def register():
-    form="form name"
-    if form.validate_on_submit():
-        roles="Users";
-        user=Users(email=form.email.data,username=form.username.data,password=form.password.data,role_id=roles)
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for("auth.login"))
-
-    return render_template("auth/register",form=form)
-
-
-
-'''
-login users
-'''
