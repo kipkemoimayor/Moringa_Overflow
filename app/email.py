976207b44acee6b8f349faf5b67f40751c440d5d
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-30 11:52:16 +0300

    added emails

--- app/email.py
+++ app/email.py
@@ -5,7 +5,7 @@ from manage import app
 import os
 
 def send_email(subject, sender, recepients, text_body, html_body):
-    msg = Message(subject,sender=sender,recipients=recepients)
+    msg = Message(subject,sender=sender,recipients=[recepients])
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
