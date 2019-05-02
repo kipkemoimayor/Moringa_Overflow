c3840bb8404405741a825ad438d6ce787ba31186
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-02 15:13:36 +0300

    migration

--- app/email.py
+++ app/email.py
@@ -13,3 +13,6 @@ def send_email(subject, sender, recepients, text_body, html_body):
 def send_reset_email(user):
     token = user.get_reset_password_token()
     send_email('Reset Password',sender="angeldilorna@gmail.com",recepients=[user.email],text_body=render_template('auth/reset_password.txt',user=user, token=token),html_body=render_template('auth/reset_password.html',user=user, token=token))
+
+
+    #.strftime("%m/%d/%Y, %H:%M:%S")
