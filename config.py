88e09ab20a356b12b7aa868f238fcc4c4ec021f0
Antavio <njuguna13@gmail.com>
2019-05-02 09:34:04 +0300

    Added a secret key to the config file.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:collins@localhost/moringa_overflow'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://antavio:qwerty@localhost/moringa_overflow'
 
-    SECRET_KEY="collo"
+    SECRET_KEY="qwerty"
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
