a95cf8769e4fd9c10cbcae9e098cc266a7d0d140
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-03 11:26:49 +0300

    resolved errors

--- config.py
+++ config.py
@@ -26,6 +26,11 @@ class ProdConfig(Config):
 
     SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
 
+class Config:
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 
 config_options={
 'development':DevConfig,
